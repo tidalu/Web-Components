bd8f6b2132f265a86bbd67fbb8afd2f8b053ca75
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2024-01-10 15:34:32 +0100

    added metadata, and attached metadata to the table's body

--- src/styled table/main.js
+++ src/styled table/main.js
@@ -1,3 +1,17 @@
 import { Table } from './components/table.js';
 
 customElements.define('styled-table', Table);
+const metaData = [
+  [1776, 'Washington, D.C.', 'USA'],
+  [1066, 'London', 'UK'],
+  [660, 'Tokyo', 'Japan'],
+  [843, 'Paris', 'France'],
+  [1788, 'Sydney', 'Australia'],
+  [1867, 'Toronto', 'Canada'],
+  [1871, 'Berlin', 'Germany'],
+  [1945, 'Seoul', 'South Korea'],
+];
+
+const userTable = document.getElementById('users');
+
+userTable.data = metaData;
